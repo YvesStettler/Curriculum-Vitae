65b3509b378f6c3c8336baf9ce56feb16b4e49c3
CptUsagi <g0rilla13666@gmail.com>
2023-01-03 11:39:35 +0100

    Finally nailed the text typing animation

--- script.js
+++ script.js
@@ -9,8 +9,32 @@ async function writingAnimation() {
 
     for (let i = 0; i < txt.length; i++) {
         textNode.innerHTML += txt.charAt(i);
-        await sleep(speed + Math.floor(Math.random * 40));
+        await sleep(speed + Math.floor(Math.random() * 40));
     }
 }
 
-writingAnimation();
+async function textCursorAnimation() {
+    const textCursor = document.getElementById('text-cursor');
+    const speed = 200;
+
+    while (true) {
+        textCursor.style.opacity = Math.abs(textCursor.style.opacity - 1)
+        await sleep(speed);
+    }
+}
+
+async function animationLoop() {
+    const textNode = document.getElementById('animated-header-text');
+    const speed = 30000;
+    const rngFactor = 30000;
+
+    while (true) {
+        writingAnimation();
+    
+        await sleep(speed + Math.floor(Math.random() * rngFactor));
+        textNode.innerHTML = "";
+    }
+}
+
+textCursorAnimation();
+animationLoop();
