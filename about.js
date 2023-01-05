eb0f9da930227e8e2b31f09be98cb774773bdb9a
CptUsagi <g0rilla13666@gmail.com>
2023-01-05 17:33:59 +0100

    position correction and started changing colors

--- about.js
+++ about.js
@@ -1,37 +1,40 @@
+const animationSpeed = 6000;
+
 const line1 = KUTE.fromTo(
     '#line1-1',
     { path: '#line1-2' },
-    { path: '#line1-1' },
-    { repeat: 999, duration: 3000, yoyo:true }
+    { path: '#line1-3' },
+    { repeat: 999, duration: animationSpeed, yoyo:true }
 );
 
 const line2 = KUTE.fromTo(
     '#line2-1',
     { path: '#line2-2' },
-    { path: '#line2-1' },
-    { repeat: 999, duration: 3000, yoyo:true }
+    { path: '#line2-3' },
+    { repeat: 999, duration: animationSpeed - 600, yoyo:true }
 );
 
 const line3 = KUTE.fromTo(
     '#line3-1',
     { path: '#line3-2' },
-    { path: '#line3-1' },
-    { repeat: 999, duration: 3000, yoyo:true }
+    { path: '#line3-3' },
+    { repeat: 999, duration: animationSpeed - 100, yoyo:true }
 );
 
 const line4 = KUTE.fromTo(
     '#line4-1',
     { path: '#line4-2' },
-    { path: '#line4-1' },
-    { repeat: 999, duration: 3000, yoyo:true }
+    { path: '#line4-3' },
+    { repeat: 999, duration: animationSpeed - 1000, yoyo:true }
 );
 
 const line5 = KUTE.fromTo(
     '#line5-1',
     { path: '#line5-2' },
-    { path: '#line5-1' },
-    { repeat: 999, duration: 3000, yoyo:true }
+    { path: '#line5-3' },
+    { repeat: 999, duration: animationSpeed - 800, yoyo:true }
 );
+
 
 line1.start();
 line2.start();
